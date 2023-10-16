8dd6f310994b490a4e504545348f58817fcefff5
Kevin Hudgens <hudgens1073@gmail.com>
2023-10-15 23:27:28 -0500

    Added the form that is going to search for the pokemon based on user search choice

---
+++ src/components/PokegetForm.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+// Form needs to say the selected search by
+// have a search bar
+
+export default function PokegetForm() {
+  const [formTitle, setFormTitle] = React.useState("By Pokemon");
+
+  return (
+    <FormControl>
+      <InputLabel htmlFor="component-outlined">{formTitle}</InputLabel>
+      <OutlinedInput
+        id="component-outlined"
+        defaultValue="Bulbasaur"
+        label="Name"
+      />
+    </FormControl>
+  );
+}
