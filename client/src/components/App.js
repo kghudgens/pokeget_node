fa9e79a1d14744693c04b9e2458e0929bf7c4402
Kevin Hudgens <hudgens1073@gmail.com>
2023-11-25 00:24:19 -0600

    created method to make api call

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,18 @@
 import HomePage from "./Home";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
-      <HomePage />
+      <h1>{message}</h1>
     </div>
   );
 }
