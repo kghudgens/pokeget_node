1429c6c01ffe03d914add5218fc90d6e4d3f8a36
Kevin Hudgens <hudgens1073@gmail.com>
2023-11-25 00:23:52 -0600

    Corrected naming convention

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -5,11 +5,12 @@ const router = express.Router();
 
 // home page
 router.get("/", (req, res) => {
+  console.log("Connected");
   res.send("home");
 });
 
 // test api
-router.get("/api", (request, result) => {
+router.get("/api", (req, res) => {
   result.json({
     message: "Hello from server",
   });
