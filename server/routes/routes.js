88959d5bd7dffcb206352547ccd0c39275265733
Kevin Hudgens <hudgens1073@gmail.com>
2023-11-24 23:23:19 -0600

    setting up landing page api

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,18 @@
-app.get("/api", (request, result) => {
+const express = require("express");
+const app = express();
+
+const router = express.Router();
+
+// home page
+router.get("/", (req, res) => {
+  res.send("home");
+});
+
+// test api
+router.get("/api", (request, result) => {
   result.json({
     message: "Hello from server",
   });
 });
+
+module.exports = router;
