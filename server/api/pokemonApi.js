f5e68b7b0fe8f300d3e9e0bcb36c152ac12c2363
Kevin Hudgens <hudgens1073@gmail.com>
2023-12-07 22:34:48 -0600

    Created method retrieve and extract needed data from api

--- server/api/pokemonApi.js
+++ server/api/pokemonApi.js
@@ -9,7 +9,7 @@ function searchByPokemonName(name) {
     .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
     .then((response) => {
       const responseData = response.data;
-      const returnedPokemon = extractValuesFromObject(responseData);
+      const returnedPokemon = extractValuesFromPokemon(responseData);
       saveSearch(returnedPokemon.getName(), returnedPokemon);
     })
     .catch((error) => {
@@ -17,7 +17,52 @@ function searchByPokemonName(name) {
     });
 }
 
-function extractValuesFromObject(data) {
+// get habitat
+function searchForAbilities(name) {
+  axios
+    .get(`https://pokeapi.co/api/v2/ability/${name}/`)
+    .then((response) => {
+      const responseData = response.data;
+      const returnedPokemonAbility = extractValuesFromAbility(responseData);
+    })
+    .catch((error) => {
+      console.log(
+        "Error in the Search By Name For Abilities api call: ",
+        error
+      );
+    });
+}
+
+function extractValuesFromAbility(data) {
+  const extractedDataMap = new Map();
+  const extractedResponseData = new Map();
+  const propertiesToExtract = ["pokemon", "effect_entries"];
+  const pokemonFromAbilities = [];
+
+  for (let i = 0; i < propertiesToExtract.length; i++) {
+    // update this switch statement if more props to extract come about
+    switch (i) {
+      case 0:
+        extractedResponseData.set("pokemon", data[propertiesToExtract[i]]);
+        break;
+      case 1:
+        extractedResponseData.set(
+          "effect_entries",
+          data[propertiesToExtract[i]]
+        );
+    }
+    console.log(extractedResponseData);
+    // need to remove teh is hidden
+    // need to remove the slot
+    // need to dig into the pokemon object
+    // need to remove language, make sure in english
+    //
+  }
+}
+
+// Types
+
+function extractValuesFromPokemon(data) {
   const extractedDataMap = new Map();
   const propertiesToExtract = ["height", "id", "weight", "name"];
   let counter = 0;
@@ -41,4 +86,4 @@ function extractValuesFromObject(data) {
   return newPokemon;
 }
 
-export { searchByPokemonName };
+export { searchByPokemonName, searchForAbilities };
